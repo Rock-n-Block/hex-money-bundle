34f7fa2fb2748ac57649eec18f89470d634b3460
rateupmaster <rateupmaster@gmail.com>
2020-06-02 18:40:12 +0300

    Update HexMoneyExchangeETH.sol

--- contracts/exchange/HexMoneyExchangeETH.sol
+++ contracts/exchange/HexMoneyExchangeETH.sol
@@ -5,48 +5,53 @@ import "../UniswapGetters/IUniswapExchangeAmountGetters.sol";
 
 
 contract HexMoneyExchangeETH is HexMoneyExchangeBase {
-
     address internal uniswapGetterInstance;
 
-
-    constructor (HXY _hxyToken, address payable _dividendsContract)
-    HexMoneyExchangeBase(_hxyToken, _dividendsContract)
-    public {
-        decimals = 10 ** 18;
-        minAmount = 10 ** 10;
-        maxAmount = SafeMath.mul(10 ** 9, decimals);
+    constructor(HXY _hxyToken, address payable _dividendsContract)
+        public
+        HexMoneyExchangeBase(_hxyToken, _dividendsContract)
+    {
+        decimals = 10**18;
+        minAmount = 10**10;
+        maxAmount = SafeMath.mul(10**9, decimals);
     }
 
     function getUniswapGetterInstance() public view returns (address) {
         return uniswapGetterInstance;
     }
 
-    function setUniswapGetterInstance(address newUniswapGetterInstance)  public onlyAdminRole {
+    function setUniswapGetterInstance(address newUniswapGetterInstance)
+        public
+        onlyAdminRole
+    {
         uniswapGetterInstance = newUniswapGetterInstance;
     }
 
     function getConvertedAmount(uint256 _amount) public view returns (uint256) {
-        return IUniswapExchangeAmountGetters(uniswapGetterInstance).getEthToTokenInputPrice(_amount);
+        return
+            IUniswapExchangeAmountGetters(uniswapGetterInstance)
+                .getEthToTokenInputPrice(_amount);
     }
 
-        // Assets Transfers
+    // Assets Transfers
     receive() external payable {
-        uint256 hexAmount = IUniswapExchangeAmountGetters(uniswapGetterInstance).getEthToTokenInputPrice(msg.value);
-
-        HXY(hxyToken).mintFromDapp(_msgSender(), hexAmount);
-        _addToDividends(msg.value);
+        _exchangeEth(msg.value);
     }
 
     function exchangeEth() public payable {
-        uint256 hexAmount = IUniswapExchangeAmountGetters(uniswapGetterInstance).getEthToTokenInputPrice(msg.value);
-
-        HXY(hxyToken).mintFromDapp(_msgSender(), hexAmount);
-        _addToDividends(msg.value);
+        _exchangeEth(msg.value);
     }
 
     function _addToDividends(uint256 _amount) internal override {
         dividendsContract.transfer(_amount);
     }
 
+    // Internal
+    function _exchangeEth(uint256 value) internal {
+        uint256 hexAmount = IUniswapExchangeAmountGetters(uniswapGetterInstance)
+            .getEthToTokenInputPrice(value);
 
+        HXY(hxyToken).mintFromDapp(_msgSender(), hexAmount);
+        _addToDividends(value);
+    }
 }
